abd3f4db00c666d09d4b1f16c411213fa07c39fd
Rodrig Mtz <roes2207@gmail.com>
2022-07-07 19:31:32 -0500

    Se termino el cuarto bloque del curso practico de Js

--- Bloque-4/analisis.js
+++ Bloque-4/analisis.js
@@ -1,17 +1,8 @@
-const salarioCol = colombia.map(
-    function (personita){
-        return personita.salary;
-    }
-);
-
-const salariosColSorted = salarioCol.sort(
-    function( a, b ){
-        return a - b;
-    }
-);
-
-
-
+// Helpers o Utils--- En programación esto se conoce como funciones que no son parte de la logiaca del código
+// pero las nesecitamos para el programa
+function esPar(numerito){
+    return (numerito % 2 === 0);
+}
 
 function calcularPromedioAritmetico(lista){             
     const sumaLista = lista.reduce(                     
@@ -24,9 +15,7 @@ function calcularPromedioAritmetico(lista){
     return promedioLista;
 };
 
-function esPar(){
-    return (numerito % 2 === 0);
-}
+// Calculadora de mediana
 
 function medianaSalario(lista) {
     const mitad = parseInt(lista.length / 2);
@@ -36,50 +25,47 @@ function medianaSalario(lista) {
         const personitaMitad1 = lista[mitad - 1];
         const personitaMitad2 = lista[mitad];
 
-        const promedio1y2 = calcularPromedioAritmetico([personitaMitad1, personitaMitad2]); 
+        const mediana = calcularPromedioAritmetico([personitaMitad1, personitaMitad2]); 
 
-        return mediana = promedio1y2;
+        return mediana;
     } else {
 
         const personitaMitad = lista[mitad];
 
-        return mediana = personitaMitad;
+        return personitaMitad;
     }
 }
 
-console.log(
-    medianaSalario(salariosColSorted)
+// Mediana General
+
+const salarioCol = colombia.map(
+    function (personita){
+        return personita.salary;
+    }
 );
 
+const salariosColSorted = salarioCol.sort(
+    function( a, b ){
+        return a - b;
+    }
+);
 
+const medianaGeneralCol = medianaSalario(salariosColSorted);
 
-// // Helpers
-// function esPar(numerito) {
-//     return (numerito % 2 === 0);
-//   }
-  
-//   function calcularMediaAritmetica(lista) {
-//     const sumaLista = lista.reduce(
-//       function (valorAcumulado = 0, nuevoElemento) {
-//         return valorAcumulado + nuevoElemento;
-//       }
-//     );
-//     const promedioLista = sumaLista / lista.length;
-//     return promedioLista;
-//   }
-  
-//   // Calculadora de mediana
-//   function medianaSalarios(lista) {
-//     const mitad = parseInt(lista.length / 2);
-  
-//     if (esPar(lista.length)) {
-//       const personitaMitad1 = lista[mitad - 1];
-//       const personitaMitad2 = lista[mitad];
-  
-//       const mediana = calcularMediaAritmetica([personitaMitad1, personitaMitad2]);
-//       return mediana;
-//     } else {
-//       const personitaMitad = lista[mitad];
-//       return personitaMitad;
-//     }
-//   }
+// Mediana del Top 10%
+
+const spliceStart = (salariosColSorted.length * 90) / 100;
+const spliceCount = salariosColSorted - spliceStart;
+
+const salariosColTop10 = salariosColSorted.splice(
+    spliceCount,
+    spliceStart,
+);
+
+const medianaTop10Col = medianaSalario(salariosColTop10)
+
+console.log({
+    medianaGeneralCol,
+    medianaTop10Col,
+
+});
